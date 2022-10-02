7f7dca80ba61689b7ce8307a18e26b56b67d67df
dukanacv <dukanacvla@gmail.com>
2022-10-02 20:24:07 +0200

    added some error handling(client)

---
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { NavigationExtras, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private router: Router, private toastr: ToastrService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      catchError(err => {
+        if (err) {
+          if (err.status === 400) {
+            if (err.error.errors) {
+              throw err.error
+            } else {
+              this.toastr.error(err.error.message, err.error.statusCode)
+            }
+          }
+          if (err.status === 401) {
+            this.toastr.error(err.error.message, err.error.statusCode)
+          }
+          if (err.status === 404) {
+            this.router.navigateByUrl("/not-found")
+          }
+          if (err.status === 500) {
+            const navExtras: NavigationExtras = { state: { error: err.error } }
+            this.router.navigateByUrl("/server-error", navExtras)
+          }
+        }
+        return throwError(err)
+      })
+    );
+  }
+}
